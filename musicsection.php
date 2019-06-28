365df38ca629e84ae75e579df6d8787369ca8c90
Gbolahan Immanuel Ogunlolu (GBolio) <gbolio4@users.noreply.github.com>
2019-06-28 03:18:24 +0100

    Initial commit

---
+++ musicsection.php
@@ -0,0 +1,15 @@
+<div class="container">
+		<h3 class="title">My Music [&amp; Discopgraphy]..</h3>  
+		<div class="team-w3ls">
+		  <div class="col-md-4 col-sm-4 col-xs-4 w3_agileits team-grid">
+			  <img class="img-w3l t1-wthree img-responsive" src="images/t3.jpg" alt="">
+		    <h5>Bettings - Shyguy</h5>
+				<p>Beetings by SHyguy :: 2017 Now Music</p>
+				<div class="social-icons agile">
+					
+					<div class="clearfix"> </div>
+				</div> 
+			</div>
+			<div class="clearfix"> </div>
+		</div>   
+	</div>
